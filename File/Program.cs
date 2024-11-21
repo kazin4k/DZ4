58c803f6c25c15b5e344bf3572cfcaa3288b3183
Amir Mullagaliev <brawlstars783860@gmail.com>
2024-11-21 16:36:12 +0300

    Добавьте файлы проекта.

---
+++ File/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Linq;
+
+namespace File
+{
+    public enum Vorchlivost
+    {
+        Низкий,
+        Средний,
+        Высокий
+    }
+
+    public struct Ded
+    {
+        public string Name;
+        public Vorchlivost Level;
+        public string[] Phrases;
+        public int Bruises;
+
+        public Ded(string name, Vorchlivost level, string[] phrases)
+        {
+            Name = name;
+            Level = level;
+            Phrases = phrases;
+            Bruises = 0;
+        }
+
+        public int CheckBadWords(params string[] badWords)
+        {
+            foreach (var phrase in Phrases)
+            {
+                foreach (var badWord in badWords)
+                {
+                    if (phrase.IndexOf(badWord, StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        Bruises++;
+                    }
+                }
+            }
+            return Bruises;
+        }
+    }
+    internal class Program
+    {
+        static void DrawDigit(int number)
+        {
+            switch (number)
+            {
+                case 0:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 1:
+                    Console.WriteLine("   #   ");
+                    Console.WriteLine("  ##   ");
+                    Console.WriteLine("   #   ");
+                    Console.WriteLine("   #   ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 2:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #     ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 3:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 4:
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("     # ");
+                    break;
+                case 5:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #     ");
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 6:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #     ");
+                    Console.WriteLine(" ####  ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 7:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("    #  ");
+                    Console.WriteLine("   #   ");
+                    Console.WriteLine("   #   ");
+                    break;
+                case 8:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                case 9:
+                    Console.WriteLine("  ###  ");
+                    Console.WriteLine(" #   # ");
+                    Console.WriteLine("  ####  ");
+                    Console.WriteLine("     # ");
+                    Console.WriteLine("  ###  ");
+                    break;
+                default:
+                    break;
+            }
+        }
+        static int CalculateArray(ref int product, out double average, params int[] array)
+        {
+            int sum = 0;
+            product = 1;
+
+            foreach (int number in array)
+            {
+                sum += number;
+                product *= number;
+            }
+
+            average = array.Length > 0 ? (double)sum / array.Length : 0;
+
+            return sum;
+        }
+        static void PrintArray(int[] array)
+        {
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            //Задание 1
+            Console.WriteLine("Задание 1");
+            Random random = new Random();
+            int[] numbers = new int[20];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(1, 101);
+            }
+
+            Console.WriteLine("Исходный массив:");
+            PrintArray(numbers);
+
+            int firstNumber, secondNumber;
+
+            Console.WriteLine("Введите первое число для обмена:");
+            while (!int.TryParse(Console.ReadLine(), out firstNumber) || !Array.Exists(numbers, element => element == firstNumber))
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите число из массива");
+            }
+
+            Console.WriteLine("Введите второе число для обмена:");
+            while (!int.TryParse(Console.ReadLine(), out secondNumber) || !Array.Exists(numbers, element => element == secondNumber))
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите число из массива");
+            }
+
+            int index1 = Array.IndexOf(numbers, firstNumber);
+            int index2 = Array.IndexOf(numbers, secondNumber);
+
+            int temp = numbers[index1];
+            numbers[index1] = numbers[index2];
+            numbers[index2] = temp;
+
+            Console.WriteLine("Измененный массив:");
+            PrintArray(numbers);
+
+
+            //Задание 2
+            Console.WriteLine("Задание 2");
+            int[] chisla = { 1, 2, 3, 4, 5 };
+            int product = 1;
+            double average;
+
+            int sum = CalculateArray(ref product, out average, numbers);
+
+            Console.WriteLine($"Сумма элементов: {sum}");
+            Console.WriteLine($"Произведение элементов: {product}");
+            Console.WriteLine($"Среднее арифметическое: {average}");
+
+
+            //Задание 3
+            Console.WriteLine("Задание 3");
+            bool running = true;
+            while (running)
+            {
+                Console.Write("Введите число (или 'exit' для выхода): ");
+                string input = Console.ReadLine();
+
+                if (input.Equals("exit", StringComparison.OrdinalIgnoreCase) || input.Equals("закрыть", StringComparison.OrdinalIgnoreCase))
+                {
+                    running = false;
+                    continue;
+                }
+
+                try
+                {
+                    int number = int.Parse(input);
+
+                    if (number >= 0 && number <= 9)
+                    {
+                        DrawDigit(number);
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Red;
+                        Console.Clear();
+                        Console.WriteLine("Ошибка: число должно быть от 0 до 9");
+                        System.Threading.Thread.Sleep(3000);
+                        Console.ResetColor();
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка: введено не число");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка: число слишком велико или слишком мало");
+                }
+
+                //Задание 4
+                Console.WriteLine("Задание 4");
+                Ded ded1 = new Ded("Дедушка Кирилл", Vorchlivost.Низкий, new string[] { "Гады!", "Что за ерунда?" });
+                Ded ded2 = new Ded("Дедушка Владимир", Vorchlivost.Средний, new string[] { "Проститутки!", "Я вас не понимаю!", "Гады!" });
+                Ded ded3 = new Ded("Дедушка Ярослав", Vorchlivost.Высокий, new string[] { "Как же вы меня достали!", "Гады!" });
+                Ded ded4 = new Ded("Дедушка Алексей", Vorchlivost.Низкий, new string[] { "Что за безобразие?" });
+                Ded ded5 = new Ded("Дедушка Николай", Vorchlivost.Средний, new string[] { "Проститутки!", "Вы не правы!", "Гады!", "Как же это бесит!" });
+
+                string[] badWords = { "проститутки", "гады" };
+
+                Console.WriteLine($"{ded1.Name} получил {ded1.CheckBadWords(badWords)} синяков.");
+                Console.WriteLine($"{ded2.Name} получил {ded2.CheckBadWords(badWords)} синяков.");
+                Console.WriteLine($"{ded3.Name} получил {ded3.CheckBadWords(badWords)} синяков.");
+                Console.WriteLine($"{ded4.Name} получил {ded4.CheckBadWords(badWords)} синяков.");
+                Console.WriteLine($"{ded5.Name} получил {ded5.CheckBadWords(badWords)} синяков.");
+
+            }
+        }
+    }
+}
---
+++ Tumakov/Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+
+namespace Tumakov
+{
+    internal class Program
+    {
+        public double Sravnenie(double x, double y)
+        {
+            return Math.Max(x, y);
+        }
+
+        public static void Zamena(ref double x, ref double y)
+        {
+            double z;
+            z = x; x = y; y = z;
+        }
+
+        public long Factorial(int n)
+        {
+            if (n < 0)
+            {
+                throw new ArgumentException("Факториал не определен для отрицательных чисел.");
+            }
+            else if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * Factorial(n - 1);
+            }
+        }
+        public bool Fact(int n, out long fact)
+        {
+            fact = 1;
+            if (n < 0)
+            {
+                Console.WriteLine("Факториал не определен для отрицательного числа\r\n");
+            }
+            try
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    checked
+                    {
+                        fact *= i;
+                    }
+                }
+                return true;
+            }
+            catch (OverflowException)
+            {
+                return false;
+            }
+        }
+
+        public static int NOD(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public static int NOD(int a, int b,int c)
+        {
+            return NOD(NOD(a, b), c);
+        }
+        static int Fibonacci(int n)
+        {
+            if (n <= 0)
+            {
+                throw new ArgumentException("Индекс должен быть положительным числом.");
+            }
+            else if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Задание 5.1
+            Program program = new Program();
+            Console.WriteLine("Задание 5.1");
+            Console.WriteLine("Введите 2 числа(в столбик)");
+            string a = Console.ReadLine();
+            string b = Console.ReadLine();
+            if (double.TryParse(a, out double a1) && double.TryParse(b, out double b1))
+            {
+            Console.WriteLine($"Наибольшее число из {a} и {b}: {program.Sravnenie(a1,b1)}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введите числа");
+            }
+            Console.ReadKey();
+
+            //Задание 5.2
+            Console.WriteLine("Задание 5.2");
+            Console.WriteLine("Введите значения 2 параметров(в столбик)");
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            Console.WriteLine($"Заначения до замены: {x}, {y}");
+            if (double.TryParse(x, out double x1) && double.TryParse(y, out double y1))
+            {
+                Zamena(ref x1, ref y1);
+                Console.WriteLine($"Значения параметров после замены: {x1}, {y1}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Введите число");
+            }
+
+            //5.3
+            Console.WriteLine("Задание 5.3");
+            Console.WriteLine("Введите число: ");
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int n))
+            {
+                if (program.Fact(n, out long fact))
+                {
+                    Console.WriteLine($"Факториал и числа {n} это {fact}\r\n");
+                }
+                else
+                {
+                    Console.WriteLine("Переполнение при вычислении факториала\r\n");
+                }
+            }
+
+
+            //Задание 5.4
+            try
+            {
+                Console.WriteLine("Задание 5.4");
+                Console.WriteLine("Введите число: ");
+                string fact = Console.ReadLine();
+                if (int.TryParse(fact, out int factorial))
+                {
+                    long result = program.Factorial(factorial);
+                    Console.WriteLine($"Факториал числа {fact}: {result}");
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка: Введите число корректно\r\n");
+                }
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Ошибка: факториал слишком большой\r\n");
+            }
+
+            //Домашнее задание
+            //Задание 5.1
+            Console.WriteLine("Задание 5.1");
+            Console.WriteLine("Введите три числа(в столбик): ");
+            string aa = Console.ReadLine();
+            string bb = Console.ReadLine();
+            string cc = Console.ReadLine();
+            if (int.TryParse(aa, out int aa1) && int.TryParse(bb, out int bb1) && int.TryParse(cc, out int cc1))
+            {
+                int maxi1 = NOD(aa1, bb1);
+                int maxi2 = NOD(aa1,bb1,cc1);
+                Console.WriteLine($"НОД для 2 чисел {aa1} и {bb1}: {maxi1}");
+                Console.WriteLine($"НОД для 3 чисел {aa1} и {bb1} и {cc1}: {maxi2}");
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: введите целые числа");
+            }
+            
+            //Задание 5.2
+            Console.WriteLine("Задание 5.2");
+            Console.WriteLine("Введите индекс члена последовательности Фибоначчи");
+            string nn = Console.ReadLine();
+            int resultat;
+            if (int.TryParse(nn, out int nn1))
+            {
+                resultat = Fibonacci(nn1);
+                Console.WriteLine($"N-й член последовательности Фибоначчи равен: {resultat}");
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
